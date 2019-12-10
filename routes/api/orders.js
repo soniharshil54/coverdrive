74709e6a5fcbfbb31719e112a8dec6ba0648ea2c
vipin224 <vipinpatel@vitae.co>
2019-12-10 17:57:48 +0530

    admins added

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -7,19 +7,19 @@ const checkAuth = require("../../middlewares/checkAuth")
 
 
 
-// GET request for count of friends of a specific user. 
- router.get('/user/:uid', user_controller.user_get_user);
+// GET request to get specific order details. 
+ router.get('/order/:oid', order_controller.order_get_order);
+
+ // GET request to get all users. 
+ router.get('/orders', order_controller.orders_get_orders);
 
 // GET request for count of friends of a specific user. 
- router.get('/countusers', user_controller.user_count_friends);
+ router.get('/countusers', user_controller.user_count_users);
+
+ // GET request for count of friends of a specific user. 
+ router.delete('/deleteuser', user_controller.user_delete_user);
 
 // GET request for count of friends requests of a specific user. 
- router.get('/countfriendsreqs/:uid', user_controller.user_count_friendReqs);
-
-// GET request for count of friends of a specific user. 
-// router.get('/countfriends/:uid', function(req,res){
-//     let uid = req.params.uid
-//     res.json({"user":uid})
-// });
+ router.put('/updateuser/:uid', user_controller.user_update_user);
 
 module.exports = router
--- routes/api/products.js
+++ routes/api/products.js
@@ -7,19 +7,19 @@ const checkAuth = require("../../middlewares/checkAuth")
 
 
 
-// GET request for count of friends of a specific user. 
+// GET request to get specific user. 
  router.get('/user/:uid', user_controller.user_get_user);
 
+ // GET request to get all users. 
+ router.get('/users', user_controller.user_get_user);
+
 // GET request for count of friends of a specific user. 
- router.get('/countusers', user_controller.user_count_friends);
+ router.get('/countusers', user_controller.user_count_users);
+
+ // GET request for count of friends of a specific user. 
+ router.delete('/deleteuser', user_controller.user_delete_user);
 
 // GET request for count of friends requests of a specific user. 
- router.get('/countfriendsreqs/:uid', user_controller.user_count_friendReqs);
-
-// GET request for count of friends of a specific user. 
-// router.get('/countfriends/:uid', function(req,res){
-//     let uid = req.params.uid
-//     res.json({"user":uid})
-// });
+ router.put('/updateuser/:uid', user_controller.user_update_user);
 
 module.exports = router
