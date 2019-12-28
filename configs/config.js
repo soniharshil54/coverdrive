cc11a0d6a353967c052baa7542333e505829be5b
soniharshil54 <sharshil43@yahoo.com>
2019-12-28 17:19:59 +0530

    status 200 set

--- configs/config.js
+++ configs/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI : "mongodb://soni:soni3360@ds353748.mlab.com:53748/coverdrive"
+    mongoURI : "mongodb://phoneuser:admin1212@127.0.0.1:27017/coverdrive?authSource=admin"
 }
 
 // mongodb://soni:soni3360@ds353748.mlab.com:53748/coverdrive
