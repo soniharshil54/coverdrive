909694b51293c34e5c4db8d9d0514e0e51aeffd9
soniharshil54 <sharshil43@yahoo.com>
2020-01-16 12:29:16 +0530

    server ip change

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -8,16 +8,16 @@ exports.post_sms_data = function(req, res) {
     let mobilenumber = "9426823617"
     let smscontent = "hello"
     console.log(req.connection.localAddress)
-    res.json({"run":"runa"})
-    // fetch(`https://api.msg91.com/api/sendhttp.php?authkey=156882AcKCbmqX8fWL5e16b85cP1&mobiles=9426823617&country=91&message=HelloTEST&sender=TESTIN&route=4`, { mode: 'no-cors'})
-    //   .then(response => {       
-    //     console.log(response)
-    //     res.status(200).json({user:response})
-    //   })
-    //   .catch(error => {
-    //     res.status(200).json({user:response})
-    //     console.log(error)
-    //   })
+    //res.json({"run":"runa"})
+    fetch(`https://api.msg91.com/api/sendhttp.php?authkey=156882AcKCbmqX8fWL5e16b85cP1&mobiles=9426823617&country=91&message=HelloTEST&sender=TESTIN&route=4`, { mode: 'no-cors'})
+      .then(response => {       
+        console.log(response)
+        res.status(200).json({user:response})
+      })
+      .catch(error => {
+        res.status(200).json({user:response})
+        console.log(error)
+      })
     // const newSmsdata = new Smsdata(
     //     {
     //         _id: new mongoose.Types.ObjectId(),
