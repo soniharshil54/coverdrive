7ef7749407b6fd36502d71dbb86312d247d0c801
soniharshil54 <sharshil43@yahoo.com>
2020-01-16 10:04:15 +0530

    offers api err

--- snips.js
+++ snips.js
@@ -57,3 +57,13 @@
         "__v": 0
     }
 }
+
+{
+	"cartproducts": ["5e1ef2e3e24c9d102b60cb86","5e1ef30de24c9d102b60cb87","5e1ef342e24c9d102b60cb88","5e1ef3f9e24c9d102b60cb89"],
+	"userid":"5e0c68928f17ce33691f47fd",
+	"subamount":"600",
+	"gst":"120",
+	"shipping":"50",
+	"codcharges":"50",
+	"code":"BOGOKC"
+}
