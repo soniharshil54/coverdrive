20dcc01d38af33592a01242718f2d5df9adf9fa1
soniharshil54 <sharshil43@yahoo.com>
2020-01-18 15:41:07 +0530

    pending to processing sms 2

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -14,6 +14,8 @@ var notification_controller = require('../../controllers/notifications');
 
  router.post('/postsmsdata', notification_controller.post_sms_data)
 
+ router.post('/postorderconfirm', notification_controller.post_order_confirm_sms)
+
 // GET request for count of friends of a specific user. 
 // router.get('/countusers', user_controller.count_users);
 
