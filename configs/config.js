2f1991ea5f0ef616c54f9422bb38051e235a29a8
soniharshil54 <sharshil43@yahoo.com>
2020-08-29 22:08:39 +0530

    remove db credentials

--- configs/config.js
+++ configs/config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    mongoURI : "mongodb://phoneuser:admin1212@127.0.0.1:27017/coverdrive?authSource=admin"
+    mongoURI : "mongodb://127.0.0.1:27017/coverdrive"
 }
 
 // mongodb://soni:soni3360@ds353748.mlab.com:53748/coverdrive
 // mongoURI : "mongodb://phoneuser:admin1212@127.0.0.1:27017/coverdrive?authSource=admin"
+
+// db.createUser({	user: "phoneuser",pwd: "admin1212",roles:[{role: "userAdminAnyDatabase" , db:"admin"}]})
